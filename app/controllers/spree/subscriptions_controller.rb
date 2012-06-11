7293bc1f62cad388ed12201186b9e1f1521e6eb2
coupling <martijn@coupling.nl>
2012-06-11 13:51:35 +0200

    Removed put messages, message if api key is nil.

--- app/controllers/spree/subscriptions_controller.rb
+++ app/controllers/spree/subscriptions_controller.rb
@@ -2,28 +2,27 @@ class Spree::SubscriptionsController < Spree::BaseController
   before_filter :load_config
 
   def create
-    puts ">>>>>>>>> #{@config[:mailchimp_api_key]}"
-    hominid ||= Hominid::API.new(@config[:mailchimp_api_key])
-    @errors = []
+    if @config[:mailchimp_api_key]
+      hominid ||= Hominid::API.new(@config[:mailchimp_api_key])
+      @errors = []
 
-    if params[:email].blank?
-      @errors << t('missing_email')
-    elsif params[:email] !~ /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
-      @errors << t('invalid_email_address')
-    else
-      begin
-        self.class.benchmark "Adding mailchimp subscriber" do
-          puts "before Sync"
-          puts ">>>> #{Spree::MailChimpSync::Sync::mc_subscription_opts}"
-          hominid.list_subscribe(@config[:mailchimp_list_id], params[:email], {})
-          puts "after Sync"
+      if params[:email].blank?
+        @errors << t('missing_email')
+      elsif params[:email] !~ /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+        @errors << t('invalid_email_address')
+      else
+        begin
+          self.class.benchmark "Adding mailchimp subscriber" do
+            hominid.list_subscribe(@config[:mailchimp_list_id], params[:email], {})
+          end
+        rescue Hominid::APIError => e
+          @errors << e
         end
-      rescue Hominid::APIError => e
-        @errors << e
       end
+    else
+      @errors << "No API key defined"
     end
-
-    @message = @errors.any? ? {:error => @errors.join(' / ')} : {:success => t(:you_have_been_subscribed)}
+    @message = @errors.any? ? {:error => @errors.join(' / ')} : {:notice => t(:you_have_been_subscribed)}
 
     redirect_to :back, :flash => @message
   end
